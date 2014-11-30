ce5d831d4074c072f9cdf6837b5218d4a7a90e70
Boris Kachulachki <kachulach@gmail.com>
2014-11-30 14:37:57 +0100

    commented echo

--- server/konekcija.php
+++ server/konekcija.php
@@ -4,5 +4,5 @@ if ($connection->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 } else 
 {
-	echo "uspesno povrzano";
+	//echo "uspesno povrzano";
 } 
