884d5b5d2bb4f2d204a402bfd6bfdff04fc6648b
emiovska <miovska.e@gmail.com>
2014-11-30 13:32:37 +0100

    new connection to remote database

--- server/konekcija.php
+++ server/konekcija.php
@@ -1,5 +1,8 @@
 <?php
-$connection = new mysqli("localhost", "root", "", "dbafinkihrb");
+$connection = new mysqli("cpanel1.gohost.mk", "cpfqeivo_hrb", "@finkihrb@", "cpfqeivo_human_behaviour");
 if ($connection->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+} else 
+{
+	echo "uspesno povrzano";
 }
